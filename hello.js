ae7bc0c41b54ce0e3ceb942a3cf3fcebdffa8af7
Azat Mardanov <azat.mardanov@gmail.com>
2013-08-27 15:27:02 -0700

    convert 4 space tab into 2 space indentation

--- hello.js
+++ hello.js
@@ -3,9 +3,9 @@ var port = 3000;
 var app = express();
 
 app.get('*', function(req, res){
-	res.end('Hello World');
+  res.end('Hello World');
 });
 
 app.listen(port, function(){
-	console.log('the server is running, please open your browser at http://localhost:%s', port);
+  console.log('the server is running, please open your browser at http://localhost:%s', port);
 });
