591c3c05ecdfe84116d46bd1976d171c5fd271c7
Azat Mardanov <azat.mardanov@gmail.com>
2013-08-27 15:08:17 -0700

    add code examples repository for Express.js Guide (http://expressjsguide.com)

---
+++ hello.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var port = 3000;
+var app = express();
+
+app.get('*', function(req, res){
+	res.end('Hello World');
+});
+
+app.listen(port, function(){
+	console.log('the server is running, please open your browser at http://localhost:%s', port);
+});
