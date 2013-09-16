1e79b514178db7c726f5ff0e3ee17f0e6bb3459d
Azat Mardanov <azat.mardanov@gmail.com>
2013-09-15 21:14:10 -0700

    add request examples

--- req/app.js
+++ req/app.js
@@ -27,11 +27,35 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
 app.get('/search', function(req, res) {
-  console.log(req.query)
+  console.log(req.query);
   res.end(JSON.stringify(req.query)+'\n');
+});
+
+app.get('/params/:role/:name/:status', function(req, res) {
+  console.log(req.params);
+  console.log(req.route);
+  res.end();
+});
+
+app.post('/body', function(req, res){
+  console.log(req.body);
+  res.end(JSON.stringify(req.body)+'\n');
+});
+
+app.post('/upload', function(req, res){
+  console.log(req.files.archive);
+  //read req.files.archive.path
+  //process the data
+  //save the data  
+  res.end();
 })
 
+app.get('/route', function(req, res){
+  console.log(req.route);
+  res.end();
+})
 app.get('/', routes.index);
 app.get('/users', user.list);
 
