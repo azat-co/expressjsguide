d604524aae665a0e9432dd9ca83691124d202977
Azat Mardanov <azat.mardanov@gmail.com>
2014-04-10 20:52:08 -0700

    remove abtesting of plans

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,32 +5,32 @@ $(document).ready(function(){
         return true
     })
     var showSample = function() {
-        $('#leanpub-main').removeClass('hide')
+        $('#leanpub-main').hide().removeClass('hide').slideDown('slow')
     }
     $('.read-sample').click(showSample)
     $('#leanpub-toc a').click(showSample)
-    var firstCta = new Cohorts.Test({
-        name: 'pro-vs-team-cta',
-        sample: 1, // we want to include all visitors in the test
-        cohorts: {
-            pro: {
-                onChosen: function() {
-                    $('.buy-team-cta').addClass('hide')
-                    $('.buy-pro-cta').removeClass('hide')
-                }
-            },
-            team: {
-                onChosen: function() {
-                    $('.buy-team-cta').removeClass('hide')
-                    $('.buy-pro-cta').addClass('hide')
-                }
-            }
-        }
-    });
-
-    $('a').click(function(e) {
-        var $target = $(e.currentTarget)
-        firstCta.event('Clicked on link'+$target.attr('href'));
-    });
+    // var firstCta = new Cohorts.Test({
+    //     name: 'pro-vs-team-cta',
+    //     sample: 1, // we want to include all visitors in the test
+    //     cohorts: {
+    //         pro: {
+    //             onChosen: function() {
+    //                 $('.buy-team-cta').addClass('hide')
+    //                 $('.buy-pro-cta').removeClass('hide')
+    //             }
+    //         },
+    //         team: {
+    //             onChosen: function() {
+    //                 $('.buy-team-cta').removeClass('hide')
+    //                 $('.buy-pro-cta').addClass('hide')
+    //             }
+    //         }
+    //     }
+    // });
+    //
+    // $('a').click(function(e) {
+    //     var $target = $(e.currentTarget)
+    //     firstCta.event('Clicked on link'+$target.attr('href'));
+    // });
 
 })
